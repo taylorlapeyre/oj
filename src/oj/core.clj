97f4054ab49a0fdb33f8d12ec37287c9481afd59
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-11-05 11:39:58 -0600

    clean up core code

--- src/oj/core.clj
+++ src/oj/core.clj
@@ -1,7 +1,8 @@
 (ns oj.core
   (:use oj.generators)
   (:require [clojure.java.jdbc :as j]
-            [oj.generators :as gen]))
+            [oj.generators :as gen]
+            [clojure.string :refer [trim]]))
 
 (def sql-select-generators
   [gen/select
@@ -23,19 +24,14 @@
 (defn sqlify
   "Takes a query map and returns a valid SQL statement to be executed."
   [query]
-  (let [generators (cond (:select query)
-                         sql-select-generators
-                         (:insert query)
-                         sql-insert-generators
-                         (:update query)
-                         sql-update-generators
-                         (:delete query)
-                         sql-delete-generators
-                         :else
-                         sql-select-generators)]
-    (clojure.string/trim (reduce str (interpose \space
-                           (for [gen generators]
-                             (gen query)))))))
+  (let [generators (cond (:select query) sql-select-generators
+                         (:insert query) sql-insert-generators
+                         (:update query) sql-update-generators
+                         (:delete query) sql-delete-generators
+                         :else sql-select-generators)]
+    (trim (reduce str (interpose \space
+                      (for [gen generators]
+                        (gen query)))))))
 
 (defn exec
   "Given a query map and a database config, generates and runs SQL for the query
@@ -55,6 +51,6 @@
                           (:delete query)) j/execute! j/query)
           tuples (jdbc-fn db [(sqlify query)])]
       (if-not (:join query) tuples
-              (for [join (:join query)]
-                (for [tuple tuples]
-                  (associate-join tuple join db)))))))
+        (for [join (:join query)]
+          (for [tuple tuples]
+            (associate-join tuple join db)))))))
