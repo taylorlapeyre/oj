4e8e8fcc940dfde9eb06eeaee248b3bba47a562a
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-11-09 14:34:00 -0600

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oj "0.1.7-SNAPSHOT"
+(defproject oj "0.1.7-SNAPSHOT2"
   :description "A refreshing way to talk to your database."
   :url "http://github.com/taylorlapeyre/oj"
   :license {:name "Eclipse Public License"
